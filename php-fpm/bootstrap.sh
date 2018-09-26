add19ba42b8ef6fba89b28fab91f90cb89e11d62
emukans <mukans.work@gmail.com>
2018-09-27 00:48:15 +0300

    Update start script

--- php-fpm/bootstrap.sh
+++ php-fpm/bootstrap.sh
@@ -11,5 +11,8 @@ bin/console doctrine:schema:update --force
 # Apply latest migrations
 php bin/console doctrine:migrations:migrate latest --no-interaction
 
+# Install assets
+php bin/console assets:install --symlink
+
 # Start FPM server
 php-fpm
--- php-fpm/initsymfony.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-composer create-project symfony/framework-standard-edition .
